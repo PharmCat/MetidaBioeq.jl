bd40eff9763ecfba87ec40220a7ddc6a3c991289
PharmCat <13901158+PharmCat@users.noreply.github.com>
2022-12-08 01:15:05 +0300

    update

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, MetidaBioeq
+#using DocumenterLaTeX
+
+makedocs(
+    modules = [MetidaBioeq],
+    sitename = "MetidaBioeq.jl",
+    authors = "Vladimir Arnautov",
+    pages = [
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(repo = "github.com/PharmCat/MetidaBioeq.jl.git", push_preview = true,
+)
