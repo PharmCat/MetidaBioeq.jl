2e6427c8bf5044473d99e9fd97badcd41fedc669
PharmCat <13901158+PharmCat@users.noreply.github.com>
2022-12-19 22:45:24 +0300

    update

--- src/MetidaBioeq.jl
+++ src/MetidaBioeq.jl
@@ -3,6 +3,8 @@ module MetidaBioeq
 using Metida, MetidaBase, MetidaNCA, GLM, MixedModels, Distributions, Tables, 
 DataFrames, CategoricalArrays
 
+using MetidaBase.StatsModels
+
 import Base: show
 
 include("types.jl")
